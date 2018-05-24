d81d0fb5c2f486c594caacb0a63250bbf67fab1a
kimimaro <kimirius@gmail.com>
2018-05-24 16:34:38 +0800

    update script.sh

--- first-network/scripts/script.sh
+++ first-network/scripts/script.sh
@@ -20,7 +20,7 @@ TIMEOUT="$4"
 LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
 COUNTER=1
 MAX_RETRY=5
-ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/hapit.com/orderers/orderer.hapit.com/msp/tlscacerts/tlsca.hapit.com-cert.pem
 
 CC_SRC_PATH="github.com/chaincode/chaincode_example02/go/"
 if [ "$LANGUAGE" = "node" ]; then
